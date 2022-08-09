810dfc2793862d7f48fd53dce11ac5b22a9d62f2
Denis Piskun <denis@convergine.com>
2022-08-09 17:24:55 +0300

    Add Recaptcha

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,104 +8,145 @@ use yii\base\Event;
 
 /**
  * CraftCMS contact form to microsoft planner
- * Version 1.0.1
+ * Version 1.0.2
  */
-
 
 
 class Plugin extends \craft\base\Plugin
 {
-    const VERSION = '1.0.1';
+	const VERSION = '1.0.2';
 
-    public $hasCpSettings=true;
+	public $hasCpSettings=true;
 
-    public function init()
-    {
+	public function init()
+	{
 
-        Event::on(Mailer::class, Mailer::EVENT_BEFORE_SEND, function (SendEvent $e) {
-            $settings = $this->getSettings();
-            $submission = $e->submission;
+		Event::on(Mailer::class, Mailer::EVENT_BEFORE_SEND, function (SendEvent $e) {
+			$settings = $this->getSettings();
+			$check_captcha = $this->checkCaptcha();
+			$e->isSpam = !$check_captcha;
+			if($check_captcha) {
 
-            $sendData = [
+				$submission = $e->submission;
 
-                'name' => $submission->fromName,
-                'email' => $submission->fromEmail,
-                'subject' => '',
-                'message' => '',
-                'phone' => '',
+				$sendData = [
 
-                'custom1' => '',
-                'custom2' => '',
+					'name'    => $submission->fromName,
+					'email'   => $submission->fromEmail,
+					'subject' => '',
+					'message' => '',
+					'phone'   => '',
 
-                'wesbite' => $this->getSiteURL(),
-                'ip' => $_SERVER['REMOTE_ADDR'],
-                'form' => $settings->formName,
-            ];
+					'custom1' => '',
+					'custom2' => '',
 
-            $submissionMessage = $submission->message;
-            if(is_array($submissionMessage)){
-                if(isset($submissionMessage['body'])) {
-                    $sendData['message'] = $submissionMessage['body'];
-                    unset($submissionMessage['body']);
-                }
+					'wesbite' => $this->getSiteURL(),
+					'ip'      => $_SERVER['REMOTE_ADDR'],
+					'form'    => $settings->formName,
+				];
 
-                if(isset($submissionMessage['fromPhone'])) {
-                    $sendData['phone'] = $submissionMessage['fromPhone'];
-                    unset($submissionMessage['fromPhone']);
-                }
+				$submissionMessage = $submission->message;
+				if ( is_array( $submissionMessage ) ) {
+					if ( isset( $submissionMessage['body'] ) ) {
+						$sendData['message'] = $submissionMessage['body'];
+						unset( $submissionMessage['body'] );
+					}
 
-                if(isset($submissionMessage['subject'])) {
-                    $sendData['subject'] = $submissionMessage['subject'];
-                    unset($submissionMessage['subject']);
-                }
+					if ( isset( $submissionMessage['fromPhone'] ) ) {
+						$sendData['phone'] = $submissionMessage['fromPhone'];
+						unset( $submissionMessage['fromPhone'] );
+					}
 
-                /*
-                 * Collecting custom fields
-                 */
-                if(count($submissionMessage)){
-                    $i=1;
-                    foreach ($submissionMessage as $k=>$v){
-                        if($i>2)
-                            break;
-                        $sendData['custom'.$i] = $v;
-                       $i++;
-                    }
-                }
-            }else{
-                $sendData['message'] = $submissionMessage;
+					if ( isset( $submissionMessage['subject'] ) ) {
+						$sendData['subject'] = $submissionMessage['subject'];
+						unset( $submissionMessage['subject'] );
+					}
 
-            }
+					/*
+					 * Collecting custom fields
+					 */
+					if ( count( $submissionMessage ) ) {
+						$i = 1;
+						foreach ( $submissionMessage as $k => $v ) {
+							if ( $i > 2 ) {
+								break;
+							}
+							$sendData[ 'custom' . $i ] = $v;
+							$i ++;
+						}
+					}
+				} else {
+					$sendData['message'] = $submissionMessage;
 
-            $client = new Client();
-            $options = [
-                'json' => $sendData,
-            ];
+				}
 
-            $res = $client->request('POST', $settings->apiURL, $options);
-            $statusCode = $res->getStatusCode();
-        });
-    }
+				$client  = new Client();
+				$options = [
+					'json' => $sendData,
+				];
 
-    protected function createSettingsModel()
-    {
-        return new SettingsModel();
-    }
+				$res        = $client->request( 'POST', $settings->apiURL, $options );
+				$statusCode = $res->getStatusCode();
+			}
+		});
+	}
 
-    protected function settingsHtml()
-    {
-        return \Craft::$app->getView()->renderTemplate('contactform-microsoft-planner/_settings',[
-            'settings'=>$this->getSettings()
-        ]);
-    }
-    /**
-     * Get site home URL
-     * @return string
-     */
-    public function getSiteURL(){
-        $baseURL = \Craft::$app->sites->primarySite->baseUrl;
-        if($baseURL === '$DEFAULT_SITE_URL'){
-            $baseURL = \Craft::parseEnv($baseURL);
-        }
-        return $baseURL;
-    }
+	protected function createSettingsModel()
+	{
+		return new SettingsModel();
+	}
+
+	protected function settingsHtml()
+	{
+		return \Craft::$app->getView()->renderTemplate('contactform-microsoft-planner/_settings',[
+			'settings'=>$this->getSettings()
+		]);
+	}
+	/**
+	 * Get site home URL
+	 * @return string
+	 */
+	public function getSiteURL(){
+		$baseURL = \Craft::$app->sites->primarySite->baseUrl;
+		if($baseURL === '$DEFAULT_SITE_URL'){
+			$baseURL = \Craft::parseEnv($baseURL);
+		}
+		return $baseURL;
+	}
+
+	public function checkCaptcha(){
+		$data = \Craft::$app->getRequest()->getParam('token');
+		$recaptcha_secret = \craft\elements\GlobalSet::find()
+		                                            ->handle('globals')
+		                                            ->one();
+
+		if(isset($recaptcha_secret->recaptchasecret)){
+			$recaptcha_secret = $recaptcha_secret->recaptchasecret;
+		}else{
+			return  true;
+		}
+
+		$base = "https://www.google.com/recaptcha/api/siteverify";
+		$params = array(
+			'secret' =>  $recaptcha_secret,
+			'response' => $data
+		);
+
+		$client = new Client();
+
+		$response = $client->request('POST', $base, ['form_params' => $params]);
+
+		if($response->getStatusCode() == 200)
+		{
+			$json = json_decode($response->getBody());
+			if($json->success)
+			{
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 }
